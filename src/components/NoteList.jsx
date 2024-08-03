a665f688f182c5bf8f562dd0d42294fb5eab5848
mildanfalah <mildanfalah@gmail.com>
2024-08-03 17:20:39 +0700

    menambahkan conditional rendering jika tidak ada catatan

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -3,6 +3,14 @@ import NoteItem from "./NoteItem";
 import PropTypes from "prop-types";
 
 function NoteList({ notes }) {
+  if (notes.length === 0) {
+    return (
+      <div className="notes-list-empty">
+        <p>Tidak ada catatan</p>
+      </div>
+    );
+  }
+
   return (
     <div className="notes-list">
       {notes.map((note) => (
